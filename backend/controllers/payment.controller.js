88edd716d5bbafc91d2b172951fb1b363b6fb507
Toronto_Beauty <zainmalikthcalhacker@gmail.com>
2025-04-26 14:50:05 +0300

    fix: updated full project and resolved errors

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -18,7 +18,7 @@ export const createCheckoutSession = async (req, res) => {
 
 			return {
 				price_data: {
-					currency: "qar", // ✅ Changed from "usd" to "qar"
+					currency: "usd",
 					product_data: {
 						name: product.name,
 						images: [product.image],
--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,42 +1,41 @@
-import jwt from "jsonwebtoken"
-import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
-    try {
-        const accessToken = req.cookies.accessToken;
-        if (!accessToken) {
-            return res.status(401).json({ message: "Unauthorized - No access token provided" })
-        }
+	try {
+		const accessToken = req.cookies.accessToken;
 
-        try {
-            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-            const user = await User.findById(decoded.userId).select("-password")
+		if (!accessToken) {
+			return res.status(401).json({ message: "Unauthorized - No access token provided" });
+		}
 
-            if (!user) {
-                return res.status(401).json({ message: "User not found" })
-            }
+		try {
+			const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+			const user = await User.findById(decoded.userId).select("-password");
 
-            req.user = user;
+			if (!user) {
+				return res.status(401).json({ message: "User not found" });
+			}
 
-            next()
-        } catch (error) {
-            if (error.name === "TokenExpirEdrror") {
-                return res.status(401).json({ message: "Unothorized - Acsess token expired" })
-            }
-            throw error
+			req.user = user;
 
-        }
-
-    } catch (error) {
-        console.log("Error in protectRoute middleware", error.message);
-        return res.status(401).json({ message: "Unothorized - Invalid acess token" });
-    }
-}
+			next();
+		} catch (error) {
+			if (error.name === "TokenExpiredError") {
+				return res.status(401).json({ message: "Unauthorized - Access token expired" });
+			}
+			throw error;
+		}
+	} catch (error) {
+		console.log("Error in protectRoute middleware", error.message);
+		return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+	}
+};
 
 export const adminRoute = (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
-        next()
-    } else {
-        return res.status(403).json({ message: "Access denied - Admin only " })
-    }
-}
+	if (req.user && req.user.role === "admin") {
+		next();
+	} else {
+		return res.status(403).json({ message: "Access denied - Admin only" });
+	}
+};
